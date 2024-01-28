e78382025c43fea3519909b7d146fb02bf1ffe8d
Jeelan Basha Shaik <jeelan.basha455@gmail.com>
2024-01-28 13:47:56 -0600

    First

--- js/my-login.js
+++ js/my-login.js
@@ -1,22 +1,3 @@
-/******************************************
- * My Login
- *
- * Bootstrap 4 Login Page
- *
- * @author          Muhamad Nauval Azhar
- * @uri 			https://nauval.in
- * @copyright       Copyright (c) 2018 Muhamad Nauval Azhar
- * @license         My Login is licensed under the MIT license.
- * @github          https://github.com/nauvalazhar/my-login
- * @version         1.2.0
- *
- * Help me to keep this project alive
- * https://www.buymeacoffee.com/mhdnauvalazhar
- * 
- ******************************************/
-
-'use strict';
-
 $(function() {
 
 	// author badge :)
